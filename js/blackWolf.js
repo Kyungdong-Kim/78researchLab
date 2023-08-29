782327ea9d7cadb26016cd7a210ea391033f0292
suhyun-yoo <wau3384@gmail.com>
2023-08-30 04:09:48 +0900

    feat : Create blackWolf file

---
+++ js/blackWolf.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // reload
+  history.scrollRestoration = "manual"
+  $('header .logo').click(function(){
+    location.href = 'https://www.78researchlab.com/';
+  });
+});
